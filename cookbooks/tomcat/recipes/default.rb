6fecbed35bda2258f9639fc4a6d8c0ed60fa1762
pandurang <ambekar10@gmail.com>
2015-01-29 15:38:04 +0530

    Added

--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -17,6 +17,7 @@ bash "install_something" do
  sudo apt-get update
  echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
  sudo apt-get install oracle-java8-set-default -y
+ sudo apt-get install maven -y
   EOH
 end
 
