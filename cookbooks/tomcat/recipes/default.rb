86f3690a1e42887f4c5563c018eec2b4e9e1e35e
pandurang <ambekar10@gmail.com>
2015-01-29 11:48:12 +0530

    Added

--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -12,11 +12,11 @@ bash "install_something" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-   sudo apt-get install software-properties-common python-software-properties -y
-   echo -ne '\n' | sudo add-apt-repository ppa:webupd8team/java
-   sudo apt-get update
-   echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-   sudo apt-get install oracle-java8-set-default
+ sudo apt-get install software-properties-common python-software-properties -y
+ echo -ne '\n' | sudo add-apt-repository ppa:webupd8team/java
+ sudo apt-get update
+ echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+ sudo apt-get install oracle-java8-set-default -y
   EOH
 end
 
