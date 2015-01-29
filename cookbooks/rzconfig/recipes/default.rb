75f46ac3d0622e12dfec2f6a26f935af238b3637
pandurang <ambekar10@gmail.com>
2015-01-29 20:01:11 +0530

    Added

--- cookbooks/rzconfig/recipes/default.rb
+++ cookbooks/rzconfig/recipes/default.rb
@@ -8,26 +8,48 @@
 #
 
 
-
-
-bash "install_something" do
+bash "install_python_sw" do
   user "root"
   cwd "/tmp"
   code <<-EOH
    sudo apt-get install python-software-properties
+  EOH
+end
+
+bash "install_nodejs" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
    echo -ne '\n' | sudo add-apt-repository ppa:chris-lea/node.js
    sudo apt-get update
    sudo apt-get install npm nodejs-legacy -y
    sudo apt-get install nodejs -y
    sudo apt-get install nodejs-dev -y
+ EOH
+end
+
+bash "install_yoman" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
    sudo npm install -g bower
    sudo npm install -g yo
    sudo npm install -g grunt-cli
    sudo apt-get install ruby1.9.1-dev -y
    sudo gem install compass
+   
+ EOH
+end
+
+
+
+
+bash "install_xvfb_ff" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
    sudo apt-get install xvfb -y
    sudo apt-get install firefox -y
-   
  EOH
 end
 
